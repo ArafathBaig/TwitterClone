7ce0d10401de1ae9960b2649bc970f2f3449d31a
Arafath Baig <arafath.moghul@gmail.com>
2019-12-26 11:49:15 +0530

    "Linear Layout commit "

--- app/src/main/java/arafath/myappcom/twitterclone/SendTweet.java
+++ app/src/main/java/arafath/myappcom/twitterclone/SendTweet.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.parse.ParseException;
@@ -14,15 +16,27 @@ import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
-public class SendTweet extends AppCompatActivity {
+public class SendTweet extends AppCompatActivity implements View.OnClickListener {
 
     EditText tweet;
+    Button viewOther;
+    ListView viewtweet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_tweet);
 
+        setTitle("Feed");
         tweet = findViewById(R.id.tweetText);
+        viewOther = findViewById(R.id.viewTweets);
+        viewtweet = findViewById(R.id.listView2);
+
+        viewOther.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
 
     }
 
